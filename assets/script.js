fd6b63c7aa8ad89c9ebe828cdbfa36d144e42a7d
unknown <smiles0721@gmail.com>
2022-02-21 20:01:29 -0500

    finshed all buttons

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ console.log('JavaScript code has loaded!')
 let yourName = "Sophia Matos" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
-let gb = 1 // Gingerbread
+let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
@@ -45,3 +45,61 @@ minusG.addEventListener("click", function(){
 }
 )
 
+
+//refrence
+let addCo = document.querySelector("#add-cc")
+
+//function add
+
+addCo.addEventListener("click", function(){
+    //window.alert("add button clicked!")
+    
+    cc = cc + 1;
+    
+    votesforChocTd.textContent = cc
+})
+
+
+// refrence 
+let minusCo = document.querySelector("#minus-cc")
+
+// function minus
+
+minusCo.addEventListener("click", function(){
+
+    if (cc > 0){
+        cc = cc - 1
+        
+        votesforChocTd.textContent = cc
+    }
+}
+)
+
+//refrence
+let addSp = document.querySelector("#add-sugar")
+
+//function add
+
+addSp.addEventListener("click", function(){
+    //window.alert("add button clicked!")
+    
+    sugar = sugar + 1;
+    
+    votesforSprinkTd.textContent = sugar
+})
+
+
+// refrence 
+let minusSp = document.querySelector("#minus-sugar")
+
+// function minus
+
+minusSp.addEventListener("click", function(){
+
+    if (sugar > 0){
+        sugar = sugar - 1
+        
+        votesforSprinkTd.textContent = sugar
+    }
+}
+)
