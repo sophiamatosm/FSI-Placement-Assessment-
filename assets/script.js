66cf07d6f82dfc3c52bacf8ce1764ec42416a2bd
unknown <smiles0721@gmail.com>
2022-02-21 19:13:20 -0500

    gingerbread

--- assets/script.js
+++ assets/script.js
@@ -8,41 +8,40 @@ let gb = 1 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
-let quantityText = document.querySelector("#qty.gb")
+let votesforGingTd = document.querySelector("#qty-gb")
+let votesforChocTd = document.querySelector("#qty-cc")
+let votesforSprinkTd = document.querySelector("#qty-sugar")
 
+console.log("Votes for ging td", votesforGingTd)
+console.log("Votes for choc td", votesforChocTd)
+console.log("Votes for sprink td", votesforSprinkTd)
 
+//refrence
+let addG = document.querySelector("#add-gb")
 
-// selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-let upButton = document.querySelector("#add-gb")
-let downButton = document.querySelector("#minus-gb")
+//function add
 
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
-
-
-// Event listener for clicks on the "+" button for Gingerbread cookies
-upButton.addEventListener("click", function(){
-    window.alert("up button clicked!")
-    gb = gb + 1
-
-    quantityText.textContent = "totalQuantity: "+ gb
+addG.addEventListener("click", function(){
+    //window.alert("add button clicked!")
+    
+    gb = gb + 1;
+    
+    votesforGingTd.textContent = gb
 })
 
-downButton.addEventListener("click", function(){
-    window.alert("down button clicked!")
-    if (gb > 0 ){
-    gb = gb - 1
-    quantityText.textContent = "totalQuantity: "+ gb
+
+// refrence 
+let minusG = document.querySelector("#minus-gb")
+
+// function minus
+
+minusG.addEventListener("click", function(){
+
+    if (gb > 0){
+        gb = gb - 1
+        
+        votesforGingTd.textContent = gb
+    }
 }
-})
+)
 
-
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-
-
-// TODO: Hook up event listeners for the rest of the buttons
